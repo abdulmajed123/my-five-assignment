6cc42cf78965ec8ca43d9f424dc49f294ec8d2ff
abdulmajed123 <abdulmajed5618@gmail.com>
2025-08-29 18:14:39 +0800

    update and added new function

--- script/script.js
+++ script/script.js
@@ -50,5 +50,47 @@ for (const button of callButton) {
 
     coins = coins - 20;
     coinsSpan.textContent = coins;
+
+    // const callHistory = document.getElementById("cart-container");
+
+    // const newCart = document.createElement("div");
+    // newCart.innerHTML = `
+    // <div class="flex justify-between items-center p-3 bg-slate-50">
+    //       <div>
+    //         <h2 class="font-bold">National Emergency Number</h2>
+    //         <p class="font-bold">999</p>
+    //       </div>
+    //       <p class="font-bold">Time</p>
+    //     </div>
+
+    // `;
+
+    // callHistory.append(newCart);
+
+    // Current time
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const time = hours + ":" + minutes + ":" + seconds;
+
+    // Create Call History entry
+    const cart = document.getElementById("cart-Container");
+    const newCart = document.createElement("div");
+    newCart.innerHTML = `
+      <div class="flex justify-between items-center p-3 bg-slate-50 mb-2 rounded-2xl">
+        <div>
+          <h2 class="font-bold">${title}</h2>
+          <p class="font-bold">${number}</p>
+        </div>
+        <p class="font-bold">${time}</p>
+      </div>
+    `;
+    cart.appendChild(newCart);
+
+    const clearHistoryBtn = document.getElementById("clear-Button");
+    clearHistoryBtn.addEventListener("click", function () {
+      newCart.innerHTML = "";
+    });
   });
 }
